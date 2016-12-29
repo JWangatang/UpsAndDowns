ff4c51b82582055997de5c4ca3f9454a98364d3d
Kelly Lampotang <klampotang@gmail.com>
2016-12-29 12:06:46 -0600

    login w/ facebook

---
+++ UpsAndDowns/UpsAndDowns/Header.h
@@ -0,0 +1,15 @@
+//
+//  Header.h
+//  UpsAndDowns
+//
+//  Created by Kelly Lampotang on 12/29/16.
+//  Copyright © 2016 Suraya Shivji. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif /* Header_h */
